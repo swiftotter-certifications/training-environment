dae5ed8fe628fa6eac98e4188efa3d2a5cfc24fb
Joseph Maxwell <joseph@swiftotter.com>
2021-11-06 12:45:20 -0500

    Updating initializer

--- app/code/Project/Bug4SlideshowBroken/Command/Initialize.php
+++ app/code/Project/Bug4SlideshowBroken/Command/Initialize.php
@@ -11,13 +11,18 @@ use Magento\Framework\App\Area;
 use Magento\Framework\App\Cache\Manager;
 use Magento\Framework\App\Config\Storage\WriterInterface;
 use Magento\Framework\App\State;
+use Magento\Framework\Filesystem\DirectoryList;
 use Magento\Framework\View\Asset\Repository;
+use Magento\Setup\Console\Command\DeployStaticContentCommand;
+use phpDocumentor\Reflection\Types\Static_;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Initialize extends Command
 {
+    const CLEAN_DIRECTORIES = ['var/page_cache', 'var/view_preprocessed', 'pub/static/frontend/'];
+
     private WriterInterface $configWriter;
 
     private Repository $assetRepository;
@@ -25,12 +30,15 @@ class Initialize extends Command
     private State $state;
     /** @var \Magento\Framework\App\Cache\Manager */
     private Manager $cacheManager;
+    private DeployStaticContentCommand $deployStaticContentCommand;
+    private DirectoryList $directoryList;
 
     public function __construct(
         WriterInterface $configWriter,
         Repository $assetRepository,
         State $state,
         Manager $cacheManager,
+        DirectoryList $directoryList,
         string $name = null
     ) {
         parent::__construct($name);
@@ -38,6 +46,7 @@ class Initialize extends Command
         $this->assetRepository = $assetRepository;
         $this->state = $state;
         $this->cacheManager = $cacheManager;
+        $this->directoryList = $directoryList;
     }
 
     protected function configure()
@@ -60,10 +69,14 @@ class Initialize extends Command
         );
 
         $this->configWriter->save('mailchimp/general/active', 1);
-        $this->configWriter->save('mailchimp/general/mailchimpjsurl', $jsAssetUrl);
+        $this->configWriter->save(base64_decode('bWFpbGNoaW1wL2dlbmVyYWwvbWFpbGNoaW1wanN1cmw='), $jsAssetUrl);
 
         $output->writeln('Clearing appropriate caches');
         $this->cacheManager->flush(['layout', 'block_html', 'full_page']);
+        foreach (static::CLEAN_DIRECTORIES as $directory) {
+            $output->writeln('Deleting ' . $this->directoryList->getRoot() . '/' . $directory);
+            exec('rm -rf ' . $this->directoryList->getRoot() . '/' . $directory);
+        }
 
         $output->writeln("<fg=black;bg=green> The update is complete and this is ready for you. You got this! </>");
     }
