7b6d326a5c3f4054f958aba5fa1f14015589856f
Joseph Maxwell <joseph@swiftotter.com>
2023-04-11 22:44:36 -0500

    Fixing DI compile

--- app/code/SwiftOtter/ProductDecorator/Plugin/Defaults/EnsurePrintSpecHasExtensionAttributes.php
+++ app/code/SwiftOtter/ProductDecorator/Plugin/Defaults/EnsurePrintSpecHasExtensionAttributes.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
 namespace SwiftOtter\ProductDecorator\Plugin\Defaults;
 
 use SwiftOtter\ProductDecorator\Api\Data\PrintSpecInterface as Target;
-use SwiftOtter\ProductDecorator\Api\Data\PrintSpecInterfaceExtensionFactory as ExtensionFactory;
+use SwiftOtter\ProductDecorator\Api\Data\PrintSpecExtensionFactory as ExtensionFactory;
 use SwiftOtter\ProductDecorator\Api\Data\PrintSpecInterfaceFactory as TargetFactory;
 
 class EnsurePrintSpecHasExtensionAttributes
